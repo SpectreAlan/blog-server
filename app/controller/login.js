293fd31a0b76ca11a101ab7ca7902e7a21aee348
SpectreAlan <htmler.cn@gmail.com>
2020-09-18 21:11:16 +0800

    修改路由

--- app/controller/login.js
+++ app/controller/login.js
@@ -5,6 +5,7 @@ const Controller = require('./base_controller');
 class LoginController extends Controller {
   async login() {
     const { ctx, service, app } = this;
+    const { username, password } = ctx.request.body;
     const validator = await app.validator.validate({ username: 'username', password: 'password', captcha: 'captcha' }, ctx.request.body);
     if (validator) {
       this.error('参数错误', validator);
@@ -14,7 +15,9 @@ class LoginController extends Controller {
     //   this.error('验证码错误');
     //   return;
     // }
-    const result = await service.database.login.login();
+    const param = { table: 'users', columns: [ 'account', 'nick_name', 'role_id', 'avatar' ], where: { account: username, user_pwd: password, status: 1 } };
+    const query = await service.sql.select(param);
+    const result = query[0];
     if (result) {
       ctx.session.role = result.role_id;
       ctx.session.username = ctx.request.body.username;
@@ -25,10 +28,12 @@ class LoginController extends Controller {
     }
   }
   async userInfo() {
-    const { service } = this;
-    const permission = await service.database.login.permission();
-    const menu = await service.database.login.menus();
-    const result = await service.api.login.permission(menu, permission);
+    const { service, ctx } = this;
+    const param = { table: 'roles', columns: [ 'role_keys' ], where: { id: ctx.session.role } };
+    const roles = await service.sql.select(param);
+    const permission = roles[0].role_keys.split(',');
+    const menu = await service.sql.selectAll({ table: 'menu' });
+    const result = await service.tools.permission(menu, permission);
     if (result) {
       this.success(result);
     } else {
@@ -39,7 +44,7 @@ class LoginController extends Controller {
     const { ctx, service } = this;
     const captcha = await service.tools.captcha();
     ctx.response.type = 'image/svg+xml';
-    ctx.body = { data: captcha.data, code: 1 };
+    this.success(captcha.data);
   }
 }
 
---
+++ app/controller/report.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const Controller = require('./base_controller');
+
+class ReportController extends Controller {
+  async dashboard() {
+    const { service } = this;
+    const images = await service.sql.selectCount('images');
+    const article = await service.sql.selectCount('article');
+    const comment = await service.sql.selectCount('comment');
+    const statistics = await service.sql.selectCount('statistics');
+    const visitors = await service.sql.select({ table: 'settings', columns: [ 'setting_content' ], where: { setting_key: 'visitors' } });
+    this.success({
+      images,
+      article,
+      comment,
+      statistics,
+      visitors: Number(visitors[0].setting_content),
+    });
+  }
+  async item() {
+    const { ctx, app } = this;
+    const key = ctx.request.body.key;
+    let month = new Date().getMonth() + 1;
+    let year = new Date().getFullYear();
+    const arr = [];
+    for (let i = 0; i < 6; i++) {
+      const l = 'select count(id) from ' + key + ' where date(create_time) between ';
+      const str = `'${year}-${month}-01' and '${year}-${month}-31'`;
+      const data = await app.mysql.query(l + str);
+      arr.push(data[0]['count(id)']);
+      --month;
+      if (month < 1) {
+        month = 12;
+        --year;
+      }
+    }
+    this.success(arr.reverse());
+  }
+  async category() {
+    const { service } = this;
+    const list = await service.sql.selectAll({ table: 'statistics', columns: [ 'browser_name', 'city_name' ] });
+    const result = await service.api.report.item(list);
+    this.success(result);
+  }
+}
+
+module.exports = ReportController;
+
