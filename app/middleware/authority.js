d5aed514c76911bb515e87796e350f98bbf832ee
SpectreAlan <htmler.cn@gmail.com>
2020-09-18 21:12:13 +0800

    编辑拦截器

--- app/middleware/authority.js
+++ app/middleware/authority.js
@@ -1,22 +1,23 @@
 'use strict';
 
-const whiteList = [ '/login', '/userInfo', '/logout' ];
+const whiteList = [ '/login', '/userInfo', '/logout', '/captcha' ];
 
 module.exports = () => {
   return async function auth(ctx, next) {
-    const url = ctx.originalUrl;
-    if (whiteList.includes(url) || url.includes('/web/')) {
-      await next();
-    } else {
-      if (ctx.session.username) {
-        if (ctx.session.api.includes(url) || url.split('/').length === 4) {
-          await next();
-        } else {
-          ctx.body = { code: 0, msg: '请联系管理员授权', data: null };
-        }
-      } else {
-        ctx.throw(401, '您需要先登陆以后才能操作');
-      }
-    }
+    await next();
+    // const url = ctx.originalUrl;
+    // if (whiteList.includes(url) || url.includes('/web/')) {
+    //   await next();
+    // } else {
+    //   if (ctx.session.username) {
+    //     if (ctx.session.api.includes(url) || url.split('/').length === 4) {
+    //       await next();
+    //     } else {
+    //       ctx.body = { code: 0, msg: '请联系管理员授权', data: null };
+    //     }
+    //   } else {
+    //     ctx.throw(401, '您需要先登陆以后才能操作');
+    //   }
+    // }
   };
 };
