22eede2062b4863218d783ea9951c11f0160f5d7
SpectreAlan <htmler.cn@gmail.com>
2020-09-18 21:10:12 +0800

    封装sql service

--- app/service/tools.js
+++ app/service/tools.js
@@ -26,6 +26,56 @@ class ToolsService extends Service {
       };
     }
   }
+  async time(format = 'yyyy-MM-dd hh:mm:ss', t) {
+    const time = t ? new Date(t) : new Date();
+    const o = {
+      'M+': time.getMonth() + 1,
+      'd+': time.getDate(),
+      'h+': time.getHours(),
+      'm+': time.getMinutes(),
+      's+': time.getSeconds(),
+      'q+': Math.floor((time.getMonth() + 3) / 3),
+      S: time.getMilliseconds(),
+    };
+
+    if (/(y+)/.test(format)) {
+      format = format.replace(RegExp.$1, (time.getFullYear() + '').substr(4 - RegExp.$1.length));
+    }
+
+    for (const k in o) {
+      if (new RegExp('(' + k + ')').test(format)) {
+        format = format.replace(
+          RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
+      }
+    }
+
+    return format;
+  }
+  async permission(menus, permission) {
+    const menu = {};
+    const menu_name = {};
+    const api = [];
+    const roles = [];
+    menus.sort((a, b) => b.menu_type - a.menu_type);
+    menus.map(item => {
+      if (item.menu_key) {
+        menu[item.id] = item.menu_key;
+        menu_name[item.id] = item.menu_name;
+      } else {
+        if (permission.includes(String(item.id))) {
+          api.push('/' + menu[item.parentId] + '/' + item.permission);
+          if (item.permission === 'search') {
+            const o = {};
+            o.title = menu_name[item.parentId];
+            o.key = menu[item.parentId];
+            roles.push(o);
+          }
+        }
+      }
+    });
+    this.ctx.session.api = api;
+    return roles;
+  }
 }
 
 module.exports = ToolsService;
