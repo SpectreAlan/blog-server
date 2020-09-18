d32aa12395228f55d4a7a6e1daf37dc476f234b0
SpectreAlan <htmler.cn@gmail.com>
2020-09-18 21:10:45 +0800

    添加路由

--- app/router.js
+++ app/router.js
@@ -5,8 +5,19 @@
  */
 module.exports = app => {
   const { router, controller } = app;
+  /**
+      ---------------------------后台管理API--------------------------------
+ */
+  // 登录
   router.get('/', controller.home.index);
   router.get('/captcha', app.controller.login.captcha);
   router.post('/login', controller.login.login);
   router.post('/userInfo', controller.login.userInfo);
+  // 报表
+  router.post('/dashboard/search', controller.report.dashboard);
+  router.post('/dashboard/search/item', controller.report.item);
+  router.get('/dashboard/search/category', controller.report.category);
+  /**
+      ---------------------------前台API--------------------------------
+ */
 };
