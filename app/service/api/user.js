de0f70453cc79434aef4d5a8d095c7339fb6a35d
划船不用桨 <jrseemail@gmail.com>
2020-12-06 16:38:47 +0800

    添加注释

--- app/service/api/user.js
+++ app/service/api/user.js
@@ -4,19 +4,21 @@ const Service = require('egg').Service;
 
 class UserService extends Service {
   permission(menus, permission) {
-    const api = [];
-    const roles = [];
-    let pids = [];
+    const api = []; // 用户有权访问的接口
+    const roles = []; // 用户有权访问的菜单id
+    let permissions = []; // 用户有权访问的菜单id
     menus.sort((a, b) => b.menu_type - a.menu_type);
+    // eslint-disable-next-line array-callback-return
     menus.map(item => {
       if (permission.includes(String(item.id))) {
         api.push(item.permission);
-        pids.push(item.parentId || item.id);
+        permissions.push(item.parentId || item.id);
       }
     });
     this.ctx.session.permission = api;
-    pids = Array.from(new Set(pids));
-    pids.map(id => {
+    permissions = Array.from(new Set(permissions));
+    // eslint-disable-next-line array-callback-return
+    permissions.map(id => { // 封装前台所需菜单信息
       const targetRoute = menus.filter(item => id === item.id)[0];
       roles.push({
         title: targetRoute.menu_name,
