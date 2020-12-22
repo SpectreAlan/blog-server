512081bb123012c434347c59641a57e4464014ea
划船不用桨 <jrseemail@gmail.com>
2020-12-22 13:21:00 +0800

    修复图片上传返回路径bug

--- app/controller/upload.js
+++ app/controller/upload.js
@@ -11,16 +11,17 @@ class UploadController extends Controller {
     const title = (path || '/blog/common/') + new Date().getTime() + '.' + type;
     const create_time = service.tools.time();
     const imageStorage = await service.tools.imageStorage(); // 获取图床
-    const url = await service[imageStorage].upload(type, file, title, create_time);
-    if (url) {
+    const res = await service[imageStorage].upload(type, file, title, create_time);
+    if (res) {
+      const image_url = 'image-base-url' + title;
       if (!storage) {
         await service.sql.insert({ table: 'images', param: {
           image_title,
-          image_url: 'image-base-url' + title,
+          image_url,
           create_time,
         } });
       }
-      this.success({ result: url, type: '上传' });
+      this.success({ result: image_url, type: '上传' });
     } else {
       this.error('上传失败');
     }
