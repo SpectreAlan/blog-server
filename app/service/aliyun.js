010bdcfdf481924daeddf90172396e1fe221830c
划船不用桨 <jrseemail@gmail.com>
2021-01-16 19:13:13 +0800

    删除key

--- app/service/aliyun.js
+++ app/service/aliyun.js
@@ -8,7 +8,7 @@ class AliyunService extends Service {
   async upload(type, file, title) {
     const { ctx, config } = this;
     if (!config.imageType.includes(type)) {
-      ctx.throw(400, '格式有误，仅支持png、jpg、jpeg、gif格式的图片');
+      ctx.throw(400, '格式有误, 仅支持png、jpg、jpeg、gif格式的图片');
     }
     const oss = new OSS(config.aliyun);
     const result = await oss.put(title, file.filepath);
